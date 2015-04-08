535fe0252276093df72f492383b9751dd3e58d49
junior201110 <junior201110@gmail.com>
2015-04-08 19:40:41 -0300

    ADCIONANDO BIBLIOTECAS

--- vendor/composer/autoload_namespaces.php
+++ vendor/composer/autoload_namespaces.php
@@ -6,5 +6,16 @@ $vendorDir = dirname(dirname(__FILE__));
 $baseDir = dirname($vendorDir);
 
 return array(
-    'Slim' => array($vendorDir . '/slim/slim'),
+    'Twig_Extensions_' => array($vendorDir . '/slim/extras/Views/Extension'),
+    'Symfony\\Component\\Translation\\' => array($vendorDir . '/symfony/translation'),
+    'Symfony\\Component\\HttpFoundation\\' => array($vendorDir . '/symfony/http-foundation'),
+    'Slim\\Extras' => array($vendorDir . '/slim/extras'),
+    'Slim' => array($vendorDir . '/slim/slim', $vendorDir . '/slim/middleware/src'),
+    'Psr\\Log\\' => array($vendorDir . '/psr/log'),
+    'Illuminate\\Support' => array($vendorDir . '/illuminate/support'),
+    'Illuminate\\Events' => array($vendorDir . '/illuminate/events'),
+    'Illuminate\\Database' => array($vendorDir . '/illuminate/database'),
+    'Illuminate\\Container' => array($vendorDir . '/illuminate/container'),
+    'Flynsarmy\\SlimMonolog' => array($vendorDir . '/flynsarmy/slim-monolog'),
+    'Carbon' => array($vendorDir . '/nesbot/carbon/src'),
 );
--- vendor/composer/autoload_psr4.php
+++ vendor/composer/autoload_psr4.php
@@ -6,4 +6,7 @@ $vendorDir = dirname(dirname(__FILE__));
 $baseDir = dirname($vendorDir);
 
 return array(
+    'Monolog\\' => array($vendorDir . '/monolog/monolog/src/Monolog'),
+    'League\\OAuth2\\Server\\' => array($vendorDir . '/league/oauth2-server/src'),
+    'League\\Event\\' => array($vendorDir . '/league/event/src'),
 );
